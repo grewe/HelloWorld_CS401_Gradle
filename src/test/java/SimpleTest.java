9c9dad74004ed2f172af7059a61e0da95f9a9f00
grewe <lynne.grewe@csueastbay.edu>
2020-01-18 15:25:42 -0800

    Add test SimpleTest.java silly test case and update .travis.yml to add "gradle check" to script

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 /**
  * Test Class containing simple tests
  */
@@ -8,5 +10,12 @@ public class SimpleTest {
     @Test
     public void test1() {
         System.out.println("Test 1 works");
+    }
+
+
+    @Test
+    public void test2() {
+        System.out.println("Test 2 fails"); //
+        fail("Not implemented yet");
     }
 }
