31b307906d7cb7196fef0ca97b8f304050c86d6a
grewe <lynne.grewe@csueastbay.edu>
2020-01-18 15:10:26 -0800

    Add test SimpleTest.java silly test case and update .travis.yml to add "gradle check" to script

--- src/main/java/HelloWorld_CS401_Gradle.java
+++ src/main/java/HelloWorld_CS401_Gradle.java
@@ -3,6 +3,6 @@ public class HelloWorld_CS401_Gradle {
 
     public static void main(String args[]){
 
-        System.out.println("Hello CS401 from an IntelliJ Gradle Java project!s");
+        System.out.println("Hello CS401 from an IntelliJ Gradle Java project!s")
     }
 }
